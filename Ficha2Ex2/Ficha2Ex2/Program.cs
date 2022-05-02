7a40b8dc2ba2b4a1cd8240130cbfc6c28f09eab7
brndz1 <96178968+brndz1@users.noreply.github.com>
2022-05-02 02:25:44 +0100

    Add files via upload

---
+++ Ficha2Ex2/Ficha2Ex2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace CalculaNota
+{
+    class Test
+    {
+        // Main  
+        static void Main(string[] args)
+        {
+            string nome;
+            float nota1;
+            float nota2;
+            float nota3;
+            float notafinal;
+
+
+            //ler o nome do aluno e a nota dos 3 testes
+            Console.Write("Introduza o seu nome : ");
+            Console.Write("Introduza a nota do primeiro teste: ");
+            Console.Write("Introduza a nota do segundo teste: ");
+            Console.Write("Introduza a nota do terceiro teste: ");
+
+            //calcula a classificação final
+            notafinal = nota1 * 0.2 + nota2 * 0.5 + nota3 * 0.3;
+
+            //determina a situação do aluno
+            if notafinal >= 10 
+                aluno aprovado
+                    else 
+                aluno reprovado
+                
+
+            //imprime nome, classificação final e situação do aluno
+            Console.WriteLine();
+            
+
+            //exit
+            Console.ReadLine();
+        }
+    }
+}
